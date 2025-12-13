abb96b2f1051d34d465a743688bb06c9f4d99581
Cayden Lunt <cayden.lunt@gmail.com>
2025-12-12 18:23:51 -0700

    added form and mood

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,6 +1,7 @@
 const HOME_BTN = document.querySelector('#home-btn');
 const NAME_IN = document.querySelector('#user_name');
 const NAME_BTN = document.querySelector("#save_name_btn");
+const MOOD_IN = document.querySelector('#user_mood');
 const HI_CONT = document.querySelector('#say-hi');
 
 try {
@@ -12,7 +13,9 @@ try {
 try {
     NAME_BTN.addEventListener("click", () => {
         const name = NAME_IN.value;
+        const mood = MOOD_IN.value;
         localStorage.setItem("username", name);
+        localStorage.setItem("mood", mood);
         showUsername()
     });
 } catch { }
@@ -22,6 +25,10 @@ try {
 } catch { }
 
 function showUsername() {
-    let user = localStorage.getItem('username')
-    HI_CONT.textContent = `Hello, ${user != null ? user : "User"}`;
+    let user = localStorage.getItem('username');
+    let mood = localStorage.getItem('mood');
+    let m = ""
+    if (mood)
+        m = `You are ${mood}`
+    HI_CONT.textContent = `Hello, ${user != null ? user : "User"}. ${m}.`;
 }
