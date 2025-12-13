b7349b7dd92f7d0d167bd96c8f7f64a58f66fd3e
Cayden Lunt <cayden.lunt@gmail.com>
2025-12-12 20:02:14 -0700

    working on week05 assignment

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -4,3 +4,9 @@ document.getElementById("lastModified").textContent = `Last Modification: ${docu
 const today = new Date();
 const year = document.querySelector("#currentyear");
 year.textContent = `${today.getFullYear()} `;
+
+const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
+const countriesLong = countries.filter((country) => country.length > 7);
+let li = countries.length
+console.log(countriesLong)
+console.log(li)
