5b3e7203cc04c74689c78a7f5ec4fcb055610ea9
Cayden Lunt <cayden.lunt@gmail.com>
2025-12-12 17:45:49 -0700

    updated to use localstorage

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,5 +1,27 @@
 const HOME_BTN = document.querySelector('#home-btn');
+const NAME_IN = document.querySelector('#user_name');
+const NAME_BTN = document.querySelector("#save_name_btn");
+const HI_CONT = document.querySelector('#say-hi');
 
-HOME_BTN.addEventListener('click', () => {
-    document.location.href = "/wdd131/project";
-});
+try {
+    HOME_BTN.addEventListener('click', () => {
+        document.location.href = "/wdd131/project";
+    });
+} catch { }
+
+try {
+    NAME_BTN.addEventListener("click", () => {
+        const name = NAME_IN.value;
+        localStorage.setItem("username", name);
+        showUsername()
+    });
+} catch { }
+
+try {
+    showUsername();
+} catch { }
+
+function showUsername() {
+    let user = localStorage.getItem('username')
+    HI_CONT.textContent = `Hello, ${user}`;
+}
